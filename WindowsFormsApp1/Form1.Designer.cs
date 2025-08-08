a5aef451be3efdb5f9c5ec63ddba68b3ec1384bf
MIKO-MILO <168565389+MIKO-MILO@users.noreply.github.com>
2025-08-08 15:50:25 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.barangID = new System.Windows.Forms.NumericUpDown();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.buttonHapus = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barangID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 57);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Cek Koneksi";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(157, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 57);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Tampilkan Data";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.textBox6);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Location = new System.Drawing.Point(12, 189);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(331, 181);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(186, 118);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 57);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Tambah Barang";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(84, 89);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(241, 20);
+            this.textBox8.TabIndex = 5;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 92);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 13);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Harga Barang";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(84, 51);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(241, 20);
+            this.textBox7.TabIndex = 3;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 54);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(69, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Berat Barang";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(84, 17);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(241, 20);
+            this.textBox6.TabIndex = 1;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Nama Barang";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox11);
+            this.groupBox4.Controls.Add(this.label14);
+            this.groupBox4.Controls.Add(this.button8);
+            this.groupBox4.Controls.Add(this.barangID);
+            this.groupBox4.Controls.Add(this.textBox9);
+            this.groupBox4.Controls.Add(this.label11);
+            this.groupBox4.Controls.Add(this.label13);
+            this.groupBox4.Controls.Add(this.textBox10);
+            this.groupBox4.Controls.Add(this.label12);
+            this.groupBox4.Location = new System.Drawing.Point(349, 189);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(345, 227);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(87, 85);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(241, 20);
+            this.textBox11.TabIndex = 14;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(10, 89);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(69, 13);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "Berat Barang";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(189, 164);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(139, 57);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "Edit";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // barangID
+            // 
+            this.barangID.Location = new System.Drawing.Point(87, 21);
+            this.barangID.Name = "barangID";
+            this.barangID.Size = new System.Drawing.Size(241, 20);
+            this.barangID.TabIndex = 12;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(87, 118);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(241, 20);
+            this.textBox9.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(10, 122);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(73, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Harga Barang";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(10, 22);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(55, 13);
+            this.label13.TabIndex = 6;
+            this.label13.Text = "ID Barang";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(87, 53);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(241, 20);
+            this.textBox10.TabIndex = 9;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(9, 56);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(72, 13);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "Nama Barang";
+            // 
+            // lstResult
+            // 
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.Location = new System.Drawing.Point(12, 75);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(682, 108);
+            this.lstResult.TabIndex = 4;
+            this.lstResult.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // buttonHapus
+            // 
+            this.buttonHapus.Location = new System.Drawing.Point(198, 448);
+            this.buttonHapus.Name = "buttonHapus";
+            this.buttonHapus.Size = new System.Drawing.Size(320, 47);
+            this.buttonHapus.TabIndex = 5;
+            this.buttonHapus.Text = "HAPUS";
+            this.buttonHapus.UseVisualStyleBackColor = true;
+            this.buttonHapus.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(706, 527);
+            this.Controls.Add(this.buttonHapus);
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Name = "Form1";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barangID)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.NumericUpDown barangID;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ListBox lstResult;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button buttonHapus;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,246 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string cnnString = "server=127.0.0.1;database=programbarang;uid=usergatot;pwd=2108;";
+        MySqlConnection myConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool MySqlConnect()
+        {
+            myConnection = new MySqlConnection(cnnString);
+            try
+            {
+                myConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi Kesalahan" + ex);
+                return false;
+            }
+        }
+
+        private void MySqlDisconnect()
+        {
+            myConnection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MySqlConnect())
+                MessageBox.Show("Terkoneksi dengan MySql");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (MySqlConnect())
+            {
+                string query = "SELECT * FROM barang_data";
+                var cmd = new MySqlCommand(query, myConnection);
+                var reader = cmd.ExecuteReader();
+
+                lstResult.Items.Clear();
+
+                while (reader.Read())
+                {
+                    uint barang_id = reader.GetUInt32(0);
+                    string barang_nama = reader.GetString(1);
+                    uint barang_berat = reader.GetUInt32(2);
+                    uint barang_harga = reader.GetUInt32(3);
+                    DateTime barang_tgl_masuk = reader.GetDateTime(4);
+                    DateTime barang_tgl_edit = reader.GetDateTime(5);
+                    lstResult.Items.Add(barang_id + " \t| " + barang_nama + " \t| " + barang_berat + " \t| " + barang_harga + " \t| " + barang_tgl_masuk + " \t| " + barang_tgl_edit + " \t| ");
+                }
+                MySqlDisconnect();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MySqlConnect())
+                MessageBox.Show("Terkoneksi dengan MySql");
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string getNama = textBox6.Text;
+            uint getBerat = uint.Parse(textBox7.Text);
+            uint getHarga = uint.Parse(textBox8.Text);
+            if (MySqlConnect())
+            {
+                string query = string.Format("INSERT INTO barang_data(barang_nama, barang_berat, barang_harga, barang_tgl_masuk, barang_tgl_edit) VALUES ('{0}' ,'{1}' ,'{2}' ,'{3}' ,'{4}')",
+                    (object)getNama,
+                    (object)getBerat,
+                    (object)getHarga,
+                    (object)DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                    (object)DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                try
+                {
+                    var cmd = new MySqlCommand(query, myConnection);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+                MySqlDisconnect();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            uint getBrgID = (uint)barangID.Value;
+            string getNama = textBox10.Text;
+            uint getBerat = uint.Parse(textBox11.Text);
+            uint getHarga = uint.Parse(textBox9.Text);
+            bool barangFound = false;
+            if (MySqlConnect())
+            {
+                string queryCheck = "SELECT id_barang FROM barang_data WHERE id_barang='" + getBrgID.ToString() + "'";
+                try
+                {
+                    var cmdCheck = new MySqlCommand(queryCheck, myConnection);
+                    var reader = cmdCheck.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        barangFound = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Barang tidak ditemukan");
+                    }
+                }
+
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+                MySqlDisconnect();
+            }
+
+
+            if (MySqlConnect())
+            {
+                string query = string.Format("UPDATE barang_data SET barang_nama='{1}', barang_berat='{2}', barang_harga='{3}', barang_tgl_edit='{4}' WHERE id_barang='{0}';",
+                    (object)getBrgID,
+                    (object)getNama,
+                    (object)getBerat,
+                    (object)getHarga,
+                    (object)DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                try
+                {
+                    var cmd = new MySqlCommand(query, myConnection);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
+                MySqlDisconnect();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+            if (lstResult.SelectedItem != null)
+            {
+                string selectedItem = lstResult.SelectedItem.ToString();
+                string[] data = selectedItem.Split('|');
+
+                if (data.Length > 0)
+                {
+                    uint id = uint.Parse(data[0].Trim());
+                    barangID.Value = id;
+                }
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            uint id = (uint)barangID.Value;
+
+            DialogResult result = MessageBox.Show(
+                $"Yakin ingin menghapus barang dengan ID {id}?",
+                "Konfirmasi Hapus",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                if (MySqlConnect())
+                {
+                    string query = "DELETE FROM barang_data WHERE id_barang = @id";
+
+                    try
+                    {
+                        using (MySqlCommand cmd = new MySqlCommand(query, myConnection))
+                        {
+                            cmd.Parameters.AddWithValue("@id", id);
+                            int rowsAffected = cmd.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                                MessageBox.Show("Data berhasil dihapus!");
+                            else
+                                MessageBox.Show("Data tidak ditemukan atau sudah dihapus.");
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Gagal menghapus data: " + ex.Message);
+                    }
+
+                    MySqlDisconnect();
+                }
+            }
+        }
+    }
+}
+
